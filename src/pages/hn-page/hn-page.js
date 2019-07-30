427221277f0994c94dfcbbeae6f2fa9224c3bf20
Miguel Romero <miromero@lendingclub.com>
2019-07-29 17:43:08 -0700

    HOC

--- src/pages/hn-page/hn-page.js
+++ src/pages/hn-page/hn-page.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios';
+import shallowequal from 'shallowequal';
+
+import withHOCPagination from './hoc-pagination';
 
 class HNPage extends React.Component {
 
@@ -11,32 +14,42 @@ class HNPage extends React.Component {
     componentDidMount() {
         axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
         .then((request) => {
-            const storiesIds15 = request.data.slice(0, 15);
-            console.log('STORIES IDS', storiesIds15);
-            const storiesPromise = storiesIds15.map(
+            this.props.setData && this.props.setData(request.data, 15);
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (!shallowequal(prevProps.itemsPage, this.props.itemsPage)) {
+            const storiesPromise = this.props.itemsPage.map(
                 (storieId) => {
                 return axios.get(`https://hacker-news.firebaseio.com/v0/item/${storieId}.json?print=pretty`)
-                .then(({ data }) => data);
+                .then(({ data }) => data)
+                .catch((request) => request);
             }
             );
-            console.log('STORIES Promises', storiesPromise);
+
             Promise.all(storiesPromise)
             .then(stories => {
                 this.setState({
                     stories
                 });
-            }).catch((error) => console.log(error));
-        })
+            });
+        }
     }
+    
     
     render() {
         if (this.state.stories.length === 0)
             return <p>Cargando historias...</p>
         return (
             <div>
+                <div>
+                    <button onClick={this.props.prevPage}>Anterior</button>
+                    <button onClick={this.props.nextPage}>Siguiente</button>
+                </div>
                 {
                     this.state.stories.map((story) => (
-                        <div>
+                        <div key={story.id}>
                             {story.title}
                         </div>
                     ))
@@ -50,5 +63,5 @@ HNPage.propTypes = {
 
 }
 
-export default HNPage
+export default withHOCPagination(HNPage);
 
---
+++ src/pages/hn-page/hoc-pagination.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default function withHOCPagination(Component) {
+    return class HOCPagination extends Component {
+
+        state = {
+            itemsPerPage: 15,
+            data: [],
+            itemsPage: [],
+            pages: 0,
+            currentPage: 0
+        }
+
+        initPaginator = (data, itemsPerPage = 15) => {
+            const pages = Math.ceil(data.length / itemsPerPage);
+            const itemsPage = data.slice(0, itemsPerPage);
+            this.setState({ pages, currentPage: 0, itemsPage, data, itemsPerPage });
+          }
+
+        setData = (data, itemsPerPage) => {
+            this.initPaginator(data, itemsPerPage)
+        }
+
+        goPage = index => {
+            const posInit = index * this.state.itemsPerPage
+            const posEnd = Math.min(posInit + this.state.itemsPerPage, this.state.data.length);
+            const itemsPage = this.state.data.slice(posInit, posEnd);
+            this.setState({ currentPage: index, itemsPage });
+        }
+          
+        nextPage = () => {
+            this.goPage(Math.min(this.state.currentPage + 1, this.state.pages));
+        }
+          
+        prevPage = () => {
+            this.goPage(Math.max(this.state.currentPage - 1, 0));
+        }
+
+        getProps = () => {
+            const { data, itemsPerPage, ...state } = this.state;
+            return {
+                ...state,
+                setData: this.setData
+            }
+        }
+    
+        render() {
+            return <Component
+                {...this.props}
+                {...this.getProps()}
+                setData={this.setData}
+                prevPage={this.prevPage}
+                nextPage={this.nextPage}
+                goPage={this.goPage}
+            />
+        }
+    }
+}
